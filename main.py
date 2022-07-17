619c0060127364ce9c1b18c22f0c21d292614192
junah201 <junah.dev@gmail.com>
2022-07-17 17:35:29 +0900

    feat : 초기 설정

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+import os
+import json
+import sys
+import datetime
+
+with open(f"config.json", "r", encoding="utf-8-sig") as json_file:
+    config = json.load(json_file)
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=config['PREFIX'],
+            intents=discord.Intents.all(),
+            sync_command=True,
+            application_id=config['APPLICATION_ID']
+        )
+
+    async def setup_hook(self):
+        await bot.tree.sync(guild=discord.Object(id=config['GUILD_ID']))
+
+    async def on_ready(self):
+        print("=========================")
+        print(f"대본 봇 Login 완료")
+        print(f"bot name : {self.user.name}")
+        print(f"bot id : {self.user.id}")
+        print(f"discord.py version : {discord.__version__}")
+        print("=========================")
+        await self.change_presence(status=discord.Status.online, activity=discord.Game("REC에서 대본 리딩"))
+
+
+bot = MyBot()
+bot.run(config['TOKEN'])
